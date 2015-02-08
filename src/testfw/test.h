2ff30bd83cd1f33c25507b261af3f97e7ea2f9ae
Ole Krüger <ole.krueger@campus.tu-berlin.de>
2015-02-08 21:27:09 +0100

    Add test framework

---
+++ src/testfw/test.h
@@ -0,0 +1,92 @@
+#ifndef HPKNX_TESTFW_TESTH
+#define HPKNX_TESTFW_TESTH
+
+#include <stdio.h>
+
+typedef enum {
+	TR_ASSERT,
+	TR_FAIL
+} __testfailreason;
+
+typedef struct {
+	__testfailreason reason;
+	const char* filename;
+	int fileline;
+	union {
+		struct {
+			const char* expression;
+		} assertion;
+		struct {
+			const char* message;
+		} user;
+	} info;
+} __testinfo;
+
+#define __testcase_name(name) __testcase_##name
+
+#define __testcase_failed(r) { __testinfo->reason = (r); __testinfo->filename = __FILE__; __testinfo->fileline = __LINE__; }
+
+inline void __testcase_pass(const char* name) {
+	printf("\033[0;32mPASS\033[0m %s\n", name);
+}
+
+inline void __testcase_fail(const char* name, const __testinfo* info) {
+	printf("\033[0;31mFAIL\033[0m %s [%s:%i]\n", name, info->filename, info->fileline);
+
+	switch (info->reason) {
+		case TR_ASSERT:
+			printf("     + Assertion failed: %s\n", info->info.assertion.expression);
+			break;
+
+		case TR_FAIL:
+			printf("     + User message: %s\n", info->info.user.message);
+			break;
+	}
+}
+
+/**
+ * Define a test case.
+ * Example:
+ * 	deftest(my_test, {
+ * 		assert(2 + 2 == 1);
+ * 	})
+ */
+#define deftest(name, code) \
+	bool __testcase_name(name)(__testinfo* __testinfo) { \
+		{ code; } \
+		return true; \
+	}
+
+/**
+ * Perform a test.
+ */
+#define runtest(name) { \
+	__testinfo info; \
+	if (__testcase_name(name)(&info)) { \
+		__testcase_pass(__STRING(name)); \
+	} else { \
+		__testcase_fail(__STRING(name), &info); \
+	} \
+}
+
+/**
+ * Assert that the given expression `expr` is true.
+ */
+#define assert(expr) { \
+	if (!(expr)) { \
+		__testcase_failed(TR_ASSERT); \
+		__testinfo->info.assertion.expression = __STRING(expr); \
+		return false; \
+	} \
+}
+
+/**
+ * Fail with a custom error message.
+ */
+#define fail(msg) { \
+	__testcase_failed(TR_FAIL); \
+	__testinfo->info.user.message = (msg); \
+	return false; \
+}
+
+#endif
