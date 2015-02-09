009be80c00e468d2b56844d35f735a661cd1e2b3
Ole Krüger <ole.krueger@campus.tu-berlin.de>
2015-02-09 23:50:54 +0100

    Add interim test for KNXnet/IP CONNECTION_REQUEST

--- src/hpknx.c
+++ src/hpknx.c
@@ -1,39 +1,43 @@
 #include "tests/testfw.h"
 #include "knx/knxnetip.h"
+#include "net/udp.h"
+#include "net/address.h"
 
 #include <stdio.h>
 #include <string.h>
 
 externtest(msgbuilder)
 
-static void msgbuilder_debug(const msgbuilder* mb) {
-	for (size_t i = 0; i < mb->used; i++) {
-		printf("%02X ", mb->buffer[i]);
-	}
+deftest(knxnetip_connreq, {
+	knxnetip_connreq packet_in;
 
-	putchar('\n');
-}
+	// Clear the empty padding
+	memset(&packet_in.control_host.address.sin_zero, 0, sizeof(packet_in.control_host.address.sin_zero));
+	memset(&packet_in.tunnel_host.address.sin_zero, 0, sizeof(packet_in.tunnel_host.address.sin_zero));
+
+	// Setup structure
+	packet_in.type = KNXNETIP_CONNREQ_TUNNEL;
+	packet_in.layer = KNXNETIP_LAYER_TUNNEL;
+	packet_in.control_host.protocol = KNXNETIP_PROTO_UDP;
+	packet_in.tunnel_host.protocol = KNXNETIP_PROTO_UDP;
+
+	// Generate
+	msgbuilder mb;
+	msgbuilder_init(&mb, 0);
+	assert(knxnetip_generate_connreq(&mb, &packet_in));
+
+	// Parse
+	knxnetip_packet packet_out;
+	assert(knxnetip_parse(mb.buffer, mb.used, &packet_out));
+	assert(packet_out.service == KNXNETIP_CONNECTION_REQUEST);
+	assert(memcmp(&packet_out.connection_request,
+	              &packet_in,
+	              sizeof(packet_in)) == 0);
+})
 
 int main(void) {
 	runtest(msgbuilder);
-
-	knxnetip_packet packet;
-
-	packet.service = KNXNETIP_CONNECTION_REQUEST;
-	packet.payload.connection_request.type = KNXNETIP_CONNREQ_TUNNEL;
-	packet.payload.connection_request.layer = KNXNETIP_LAYER_TUNNEL;
-
-	memset(&packet.payload.connection_request.control_host, 0,
-	       sizeof(packet.payload.connection_request.control_host));
-	memset(&packet.payload.connection_request.tunnel_host, 0,
-	       sizeof(packet.payload.connection_request.tunnel_host));
-
-	packet.payload.connection_request.control_host.protocol = KNXNETIP_PROTO_UDP;
-	packet.payload.connection_request.tunnel_host.protocol = KNXNETIP_PROTO_UDP;
-
-	msgbuilder mb;
-	knxnetip_generate(&mb, &packet);
-	msgbuilder_debug(&mb);
+	runtest(knxnetip_connreq);
 
 	return 0;
 }
