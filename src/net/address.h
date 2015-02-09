61cf3ea52938cd25132849b0b4f878b9d4729740
Ole Krüger <ole.krueger@campus.tu-berlin.de>
2015-02-09 22:45:35 +0100

    Shorten ip4address to ip4addr

--- src/net/address.h
+++ src/net/address.h
@@ -8,13 +8,13 @@
 /**
  * IPv4 Address
  */
-typedef struct sockaddr_in ip4address;
+typedef struct sockaddr_in ip4addr;
 
 /**
  * Generate an IPv4 address from a string and a given port.
  */
-inline void ip4address_from_string(ip4address* addr, const char* addrstr, uint16_t port) {
-	memset(addr, 0, sizeof(ip4address));
+inline void ip4addr_from_string(ip4addr* addr, const char* addrstr, uint16_t port) {
+	memset(addr, 0, sizeof(ip4addr));
 	inet_pton(AF_INET, addrstr, &addr->sin_addr);
 	addr->sin_port = htons(port);
 }
