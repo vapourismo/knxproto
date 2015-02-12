acb9ae19cf92bdda57f4a27232a98bddc6774d2e
Ole Krüger <ole.krueger@campus.tu-berlin.de>
2015-02-12 20:20:27 +0100

    Remove knxnetip_append_service

--- src/knx/header.c
+++ src/knx/header.c
@@ -1,4 +1,5 @@
 #include "header.h"
+#include <arpa/inet.h>
 
 /* A KNXnet/IP frame is a header followed by a payload.
  * The header follows this structure:
@@ -10,71 +11,6 @@
  *
  * All integers are in network byte order (big-endian).
  */
-
-static const uint8_t knxnetip_search_request_data[2]               = {0x02, 0x01};
-static const uint8_t knxnetip_search_response_data[2]              = {0x02, 0x02};
-static const uint8_t knxnetip_description_request_data[2]          = {0x02, 0x03};
-static const uint8_t knxnetip_description_response_data[2]         = {0x02, 0x04};
-static const uint8_t knxnetip_connection_request_data[2]           = {0x02, 0x05};
-static const uint8_t knxnetip_connection_response_data[2]          = {0x02, 0x06};
-static const uint8_t knxnetip_connectionstate_request_data[2]      = {0x02, 0x07};
-static const uint8_t knxnetip_connectionstate_response_data[2]     = {0x02, 0x08};
-static const uint8_t knxnetip_disconnect_request_data[2]           = {0x02, 0x09};
-static const uint8_t knxnetip_disconnect_response_data[2]          = {0x02, 0x0A};
-static const uint8_t knxnetip_device_configuration_request_data[2] = {0x03, 0x10};
-static const uint8_t knxnetip_device_configuration_ack_data[2]     = {0x03, 0x11};
-static const uint8_t knxnetip_tunnel_request_data[2]               = {0x04, 0x20};
-static const uint8_t knxnetip_tunnel_response_data[2]              = {0x04, 0x21};
-static const uint8_t knxnetip_routing_indication_data[2]           = {0x05, 0x30};
-
-static bool knxnetip_append_service(msgbuilder* mb, knxnetip_service srv) {
-	switch (srv) {
-		case KNXNETIP_SEARCH_REQUEST:
-			return msgbuilder_append(mb, knxnetip_search_request_data, 2);
-
-		case KNXNETIP_SEARCH_RESPONSE:
-			return msgbuilder_append(mb, knxnetip_search_response_data, 2);
-
-		case KNXNETIP_DESCRIPTION_REQUEST:
-			return msgbuilder_append(mb, knxnetip_description_request_data, 2);
-
-		case KNXNETIP_DESCRIPTION_RESPONSE:
-			return msgbuilder_append(mb, knxnetip_description_response_data, 2);
-
-		case KNXNETIP_CONNECTION_REQUEST:
-			return msgbuilder_append(mb, knxnetip_connection_request_data, 2);
-
-		case KNXNETIP_CONNECTION_RESPONSE:
-			return msgbuilder_append(mb, knxnetip_connection_response_data, 2);
-
-		case KNXNETIP_CONNECTIONSTATE_REQUEST:
-			return msgbuilder_append(mb, knxnetip_connectionstate_request_data, 2);
-
-		case KNXNETIP_CONNECTIONSTATE_RESPONSE:
-			return msgbuilder_append(mb, knxnetip_connectionstate_response_data, 2);
-
-		case KNXNETIP_DISCONNECT_REQUEST:
-			return msgbuilder_append(mb, knxnetip_disconnect_request_data, 2);
-
-		case KNXNETIP_DISCONNECT_RESPONSE:
-			return msgbuilder_append(mb, knxnetip_disconnect_response_data, 2);
-
-		case KNXNETIP_DEVICE_CONFIGURATION_REQUEST:
-			return msgbuilder_append(mb, knxnetip_device_configuration_request_data, 2);
-
-		case KNXNETIP_DEVICE_CONFIGURATION_ACK:
-			return msgbuilder_append(mb, knxnetip_device_configuration_ack_data, 2);
-
-		case KNXNETIP_TUNNEL_REQUEST:
-			return msgbuilder_append(mb, knxnetip_tunnel_request_data, 2);
-
-		case KNXNETIP_TUNNEL_RESPONSE:
-			return msgbuilder_append(mb, knxnetip_tunnel_response_data, 2);
-
-		case KNXNETIP_ROUTING_INDICATION:
-			return msgbuilder_append(mb, knxnetip_routing_indication_data, 2);
-	}
-}
 
 bool knxnetip_append_header(msgbuilder* mb, knxnetip_service srv, uint16_t length) {
 	// This preamble will always be there,
@@ -95,8 +31,11 @@ bool knxnetip_append_header(msgbuilder* mb, knxnetip_service srv, uint16_t lengt
 	// Reserve space for future calls to `msgbuilder_append`.
 	msgbuilder_reserve(mb, length);
 
+	// Generate service descriptor
+	uint16_t u16srv = htons(srv);
+
 	return
 		msgbuilder_append(mb, preamble, 2) &&
-		knxnetip_append_service(mb, srv) &&
+		msgbuilder_append(mb, (const uint8_t *) &u16srv, 2) &&
 		msgbuilder_append(mb, length_data, 2);
 }
