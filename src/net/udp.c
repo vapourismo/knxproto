c01227658a658061f250d418e86e9b6f657ad734
Ole Krüger <ole.krueger@campus.tu-berlin.de>
2015-02-13 03:03:47 +0100

    Add several features to the udp source files

--- src/net/udp.c
+++ src/net/udp.c
@@ -14,3 +14,37 @@ int udpsock_create(const ip4addr* local) {
 
 	return sock;
 }
+
+ssize_t udpsock_send(int sock, const void* data, size_t size, const ip4addr* remote) {
+	size_t sent = 0;
+
+	do {
+		ssize_t tmp = sendto(sock, data, size, 0,
+		                     (const struct sockaddr*) remote, sizeof(ip4addr));
+
+		if (tmp < 0)
+			return tmp;
+
+		sent += tmp;
+	} while (sent < size);
+
+	return sent;
+}
+
+ssize_t udpsock_recv_from(int sock, void* buffer, size_t size, const ip4addr* remote) {
+	ip4addr other;
+	socklen_t other_len = sizeof(other);
+
+	while (true) {
+		ssize_t r = recvfrom(sock, buffer, size, 0, (struct sockaddr*) &other, &other_len);
+
+		if (r < 0)
+			return r;
+
+		if (other_len == sizeof(ip4addr) &&
+		    other.sin_family == remote->sin_family &&
+		    other.sin_addr.s_addr == remote->sin_addr.s_addr &&
+		    other.sin_port == remote->sin_port)
+		    	return r;
+	}
+}
--- src/net/udp.h
+++ src/net/udp.h
@@ -8,6 +8,7 @@
 
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 
 /**
  * Create a new UDP socket.
@@ -17,35 +18,40 @@ int udpsock_create(const ip4addr* local);
 /**
  * Send something through the socket.
  */
-inline ssize_t udpsock_send(int sock, const void* data, size_t size, const ip4addr* remote) {
-	size_t sent = 0;
-
-	do {
-		ssize_t tmp = sendto(sock, data, size, 0,
-		                     (const struct sockaddr*) remote, sizeof(ip4addr));
-
-		if (tmp < 0)
-			return tmp;
-
-		sent += tmp;
-	} while (sent < size);
-
-	return sent;
-}
+ssize_t udpsock_send(int sock, const void* data, size_t size, const ip4addr* remote);
 
 /**
  * Receive a datagram from anybody.
  */
 inline ssize_t udpsock_recv(int sock, void* buffer, size_t size) {
-	return recvfrom(sock, buffer, size, 0,
-	                NULL, NULL);
+	return recvfrom(sock, buffer, size, 0, NULL, NULL);
 }
+
+/**
+ * Receive a datagram from from specific remote address.
+ */
+ssize_t udpsock_recv_from(int sock, void* buffer, size_t size, const ip4addr* remote);
 
 /**
  * Close the socket.
  */
 inline void udpsock_close(int sock) {
 	close(sock);
+}
+
+/**
+ * Check if data is available. Wait a maximum of `timeout_sec` seconds for an answer.
+ */
+inline bool udpsock_select(int sock, time_t timeout_sec) {
+	fd_set fds;
+	FD_ZERO(&fds);
+	FD_SET(sock, &fds);
+
+	struct timeval tm;
+	tm.tv_sec = timeout_sec;
+	tm.tv_usec = 0;
+
+	return select(sock + 1, &fds, NULL, NULL, &tm) > 0;
 }
 
 #endif
