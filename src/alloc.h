566befa3c359cf5298b536abb4c019be71eec903
Ole Krüger <ole.krueger@campus.tu-berlin.de>
2015-02-13 18:07:48 +0100

    Add docstrings

--- src/alloc.h
+++ src/alloc.h
@@ -3,9 +3,25 @@
 
 #include <malloc.h>
 
+/**
+ * Allocate a new instance of `t`.
+ */
 #define new(t) ((t*) malloc(sizeof(t)))
+
+/**
+ * Allocate an array of `n` instances of `t`.
+ */
 #define newa(t, n) ((t*) malloc(sizeof(t) * (n)))
+
+/**
+ * Reallocate an array of `n` instances of `t`.
+ */
 #define renewa(p, t, n) ((t*) realloc(p, sizeof(t) * (n)))
+
+/**
+ * Define an anonymous array of `t`s.
+ * This uses a C99 feature and can only be used to pass them to functions.
+ */
 #define anona(t, ...) ((t[]) {__VA_ARGS__})
 
 #endif
