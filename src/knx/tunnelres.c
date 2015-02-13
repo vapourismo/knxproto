82d658b794059d161dceabb07c410a236e92c76f
Ole Krüger <ole.krueger@campus.tu-berlin.de>
2015-02-13 02:27:30 +0100

    Implement tunnel response

--- src/knx/tunnelres.c
+++ src/knx/tunnelres.c
@@ -1,13 +1,30 @@
 #include "tunnelres.h"
 #include "header.h"
 
+// Tunnel Response:
+//   Octet 0: Structure length
+//   Octet 1: Channel
+//   Octet 2: Sequence number
+//   Octet 3: Status
+
 bool knxnetip_append_tunnel_response(msgbuilder* mb,
                                      const knxnetip_tunnel_response* res) {
-	return false;
+	const uint8_t info[4] = {4, res->channel, res->seq_number, res->status};
+
+	return
+		knxnetip_append_header(mb, KNXNETIP_TUNNEL_RESPONSE, 4) &&
+		msgbuilder_append(mb, info, 4);
 }
 
 bool knxnetip_parse_tunnel_response(const uint8_t* message, size_t length,
                                     knxnetip_tunnel_response* res) {
-	return false;
+	if (length < 4 || message[0] != 4)
+		return false;
+
+	res->channel = message[1];
+	res->seq_number = message[2];
+	res->status = message[3];
+
+	return true;
 }
 
