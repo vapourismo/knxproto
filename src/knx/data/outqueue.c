151b04d7c3e4b516896f882717db465995f3dcfb
Ole Krüger <ole@vprsm.de>
2015-02-15 01:02:26 +0100

    Add copyright notice

--- src/knx/data/outqueue.c
+++ src/knx/data/outqueue.c
@@ -1,3 +1,24 @@
+/* KNX Client Library
+ * A library which provides the means to communicate with several
+ * KNX-related devices or services.
+ *
+ * Copyright (C) 2014, Ole Krüger <ole@vprsm.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include "outqueue.h"
 #include "../../alloc.h"
 
