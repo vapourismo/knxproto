a8f4c85418bbec35a4ee2ecfce871b30256803b6
Ole Krüger <ole@vprsm.de>
2015-02-18 00:54:37 +0100

    Do not double assign service field on packets

--- src/proto/knxnetip.c
+++ src/proto/knxnetip.c
@@ -38,47 +38,38 @@ bool knx_parse(const uint8_t* message, size_t length,
 	packet->service = (message[2] << 8) | message[3];
 	switch (packet->service) {
 		case KNX_CONNECTION_REQUEST:
-			packet->service = KNX_CONNECTION_REQUEST;
 			return knx_parse_connection_request(payload_ptr, payload_length,
 			                                    &packet->payload.conn_req);
 
 		case KNX_CONNECTION_RESPONSE:
-			packet->service = KNX_CONNECTION_RESPONSE;
 			return knx_parse_connection_response(payload_ptr, payload_length,
 			                                     &packet->payload.conn_res);
 
 		case KNX_CONNECTION_STATE_REQUEST:
-			packet->service = KNX_CONNECTION_STATE_REQUEST;
 			return knx_parse_connection_state_request(payload_ptr, payload_length,
 			                                          &packet->payload.conn_state_req);
 
 		case KNX_CONNECTION_STATE_RESPONSE:
-			packet->service = KNX_CONNECTION_STATE_RESPONSE;
 			return knx_parse_connection_state_response(payload_ptr, payload_length,
 			                                           &packet->payload.conn_state_res);
 
 		case KNX_DISCONNECT_REQUEST:
-			packet->service = KNX_DISCONNECT_REQUEST;
 			return knx_parse_disconnect_request(payload_ptr, payload_length,
 			                                    &packet->payload.dc_req);
 
 		case KNX_DISCONNECT_RESPONSE:
-			packet->service = KNX_DISCONNECT_RESPONSE;
 			return knx_parse_disconnect_response(payload_ptr, payload_length,
 			                                     &packet->payload.dc_res);
 
 		case KNX_TUNNEL_REQUEST:
-			packet->service = KNX_TUNNEL_REQUEST;
 			return knx_parse_tunnel_request(payload_ptr, payload_length,
 			                                &packet->payload.tunnel_req);
 
 		case KNX_TUNNEL_RESPONSE:
-			packet->service = KNX_TUNNEL_RESPONSE;
 			return knx_parse_tunnel_response(payload_ptr, payload_length,
 			                                 &packet->payload.tunnel_res);
 
 		case KNX_ROUTING_INDICATION:
-			packet->service = KNX_ROUTING_INDICATION;
 			return knx_parse_routing_indication(payload_ptr, payload_length,
 			                                    &packet->payload.routing_ind);
 
