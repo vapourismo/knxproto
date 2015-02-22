09e72ed3396e5f20cf15156ece5ab80eaec5c1bb
Ole Krüger <ole@vprsm.de>
2015-02-22 04:06:41 +0100

    test: Remove logging include

--- test/all.c
+++ test/all.c
@@ -1,5 +1,4 @@
 #include "testfw.h"
-#include "../src/util/log.h"
 
 externtest(knxnetip)
 externtest(cemi)
@@ -10,8 +9,6 @@ deftest(all, {
 })
 
 int main(void) {
-	log_setup(NULL, LOG_DEBUG);
-
 	runtest(all);
 	return 0;
 }
