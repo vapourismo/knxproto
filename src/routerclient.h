349d7375867a602eddc3638f48c5bc621895c828
Ole Krüger <ole@vprsm.de>
2015-03-03 18:37:29 +0100

    doc: Update several docstrings in routerclient

--- src/routerclient.h
+++ src/routerclient.h
@@ -31,7 +31,7 @@
 #include <pthread.h>
 
 /**
- *  Router Client
+ * Router Client
  */
 typedef struct {
 	int sock;
@@ -41,39 +41,48 @@ typedef struct {
 } knx_router_client;
 
 /**
- * Join the router's multicast group.
+ * Join the router's multicast group. This function returns true,
+ * if the socket has been created successfully and the mulicast group
+ * has been joined.
+ * It also tries to disable the mulicast loop back, but does not depend
+ * on the success or failure of that operation.
  */
 bool knx_router_connect(knx_router_client* client, const ip4addr* router);
 
 /**
- * Leave the router's multicast group.
+ * Leave the router's multicast group and close the underlying socket.
+ * If the multicast group could not be left (due to a lack of membership) this
+ * routine will return false, otherwise true.
  */
 bool knx_router_disconnect(const knx_router_client* client);
 
 /**
- * Retrieve an incoming message. The resulting buffer has to be free.
+ * Retrieve an incoming message. The resulting buffer has to be deallocated using `free`.
  * In case of success, it will return the buffer size, otherwise a negative integer;
  */
 ssize_t knx_router_recv(const knx_router_client* client, uint8_t** buffer, bool block);
 
 /**
- * Retrieve incoming an L_Data indication. You have to free the returned pointer manually.
+ * Retrieve incoming an L_Data indication. You have to `free` the returned pointer manually.
  * Returning a NULL pointer indicates a failure.
  */
 knx_ldata* knx_router_recv_ldata(const knx_router_client* client, bool block);
 
 /**
- * Send a message.
+ * Send a raw message within a routing indication.
+ * Returns true if the message has been properly transmitted.
  */
 bool knx_router_send(const knx_router_client* client, const uint8_t* payload, uint16_t length);
 
 /**
- * Send a L_Data frame.
+ * Send a L_Data indication frame.
+ * Returns true if the frame has been properly transmitted.
  */
 bool knx_router_send_ldata(const knx_router_client* client, const knx_ldata* ldata);
 
 /**
- * Sent a L_Data frame using the given destination group address and TPDU.
+ * Sent a L_Data indication frame using the given destination group address and TPDU.
+ * Returns true if the frame has been properly transmitted.
  */
 bool knx_router_send_tpdu(const knx_router_client* client, knx_addr dest, const uint8_t* tpdu, size_t length);
 
